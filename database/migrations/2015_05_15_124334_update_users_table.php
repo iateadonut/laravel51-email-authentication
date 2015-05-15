562ff460844049b4f1b2efc20b2eb420385c5a25
dan <edenwired@gmail.com>
2015-05-15 21:51:27 +0900

    migration: add email verification fields to users table

---
+++ database/migrations/2015_05_15_124334_update_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->string('activation_code')->after('password');
+			$table->boolean('active')
+				->default(0)->after('activation_code');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->dropColumn('activation_code');
+			$table->dropColumn('active');
+		});
+	}
+
+}
